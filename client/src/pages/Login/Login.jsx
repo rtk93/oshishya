9ebc8594db094e3dee62c0c46601460525d686c1
rtk93 <32133757+rtk93@users.noreply.github.com>
2023-10-25 18:56:50 +0530

    Update Login.jsx

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://oshishya-backend.onrender.com/login",
+        "https://api-oshishya.onrender.com/login",
         {
           ...inputValue,
         },
